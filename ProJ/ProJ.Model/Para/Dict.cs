d3f5f50db0fead4c22b2cc4c35cb268c59934dfc
zhangyong <673966801@qq.com>
2018-07-26 16:33:44 +0800

    词典CURD

---
+++ ProJ/ProJ.Model/Para/Dict.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProJ.Model.Para
+{
+    /// <summary>
+    /// 新建词典
+    /// </summary>
+    public class DictNew
+    {
+
+        /// <summary>
+        /// 词典类型
+        /// </summary>
+        public PublicEnum.DictType DictType { get; set; }
+        /// <summary>
+        /// 词典名称
+        /// </summary>
+        public string DictName { get; set; }
+        /// <summary>
+        /// 词典值
+        /// </summary>
+        public string DictValue { get; set; }
+
+    }
+    /// <summary>
+    /// 修改词典信息
+    /// </summary>
+    public class EidtDict : DictNew
+    {
+        /// <summary>
+        /// ID
+        /// </summary>
+        public Guid ID { get; set; }
+    }
+
+    /// <summary>
+    /// 词典查询
+    /// </summary>
+    public class DictQuery
+    {
+        /// <summary>
+        /// 词典类型
+        /// </summary>
+        public PublicEnum.DictType DictType { get; set; }
+    }
+}
